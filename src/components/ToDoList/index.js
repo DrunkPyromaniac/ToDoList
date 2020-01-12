301f1d3f722b9c5579c59835eaa4320a43c17c02
Greg J Arnold <greg@arnoldassociates.com>
2020-01-12 10:44:21 -0500

    Add in stylesheets.

--- src/components/ToDoList/index.js
+++ src/components/ToDoList/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 import ToDoListItem from 'components/ToDoListItem';
 
+import styles from './styles.module.scss';
+
 export class ToDoList extends React.Component
   {
 	render()
 		{
-		return this.props.items.map((item) =>
-			{
-			return <ToDoListItem key={item.id} {...item} />;
-			});
+		return (
+			<div className={styles.container}>
+				{
+				this.props.items.map(item =>
+					{
+					return <ToDoListItem key={item.id} {...item} />;
+					})
+				}
+			</div>
+		);
 		}
 	}
 
--- src/components/ToDoListItem/index.js
+++ src/components/ToDoListItem/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from './styles.module.scss';
+
 export class ToDoListItem extends React.Component
   {
 	render()
 		{
-		return <div>{this.props.title}</div>;
+		return <div className={styles.container}>{this.props.title}</div>;
 		}
 	}
 
