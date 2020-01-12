efe6d82d1967ca4ec6391426edb6f0e6fc5c639a
Greg J Arnold <greg@arnoldassociates.com>
2020-01-12 11:17:19 -0500

    Add more styling on the items.

--- src/components/ToDoListItem/index.js
+++ src/components/ToDoListItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styles from './styles.module.scss';
+import defaultImage from './default.png';
 
 export class ToDoListItem extends React.Component
   {
@@ -8,7 +9,7 @@ export class ToDoListItem extends React.Component
 		{
 		return (
 			<div className={styles.container}>
-				<div className={styles.image}>A picture</div>
+				<div className={styles.image}><img src={defaultImage} /></div>
 				<div className={styles.title}>{this.props.title}</div>
 				<div className={styles.category}>#octothorpe</div>
 				<div className={styles.text}>{this.props.text}</div>
